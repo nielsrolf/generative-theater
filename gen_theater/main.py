8348abede7b3b52ab358aca190172e6285ec904e
Niels Warncke <niels.warncke@gmail.com>
2022-04-10 15:20:39 +0200

    play audio immediately implemented

--- gen_theater/main.py
+++ gen_theater/main.py
@@ -1,6 +1,8 @@
+from dataclasses import dataclass
 from glob import glob
 import os
 from typing import List
+from xmlrpc.client import boolean
 import nlpcloud
 import deepl 
 import click
@@ -8,7 +10,13 @@ import click
 from gen_theater import secrets
 
 
+voices = {
+    "Luzia": "Anna",
+    # "Marin": "Markus",
+    # "Kriemhild": "Yannick"
+}
 
+@dataclass
 class Part:
     """Class for one part of the story - i.e. one line of the script
     
@@ -18,24 +26,38 @@ class Part:
         - media: one of ['audio', 'video', 'text']
         - text: the text of the part
     """
-    def __init__(self, raw: str):
-        self.raw = raw
-        self.actor, raw = raw.split(" (", 1)
-        self.media, self.text = raw.split("): ", 1)
-        self.text = self.text.strip()
+    raw: str
+    actor: str
+    media: str
+    text: str
+    generated: boolean = False
+    voice: str = "Anna"
+    
+    @staticmethod
+    def parse(raw: str):
+        actor, raw = raw.split(" (", 1)
+        media, text = raw.split("): ", 1)
+        text = text.strip()
+        return Part(raw, actor, media, text, False, voices.get(actor, "Anna"))
     
     def __str__(self):
         return f"{self.actor}: {self.text}"
+    
+    def translate(self):
+        text_de = translate(self.text)
+        return Part(self.raw, self.actor, self.media, text_de, self.generated, self.voice)
 
 
 def fill_template_gpt(parts: List[Part]) -> List[Part]:
     """Replace <generate> with generated text from GPT"""
-    client = nlpcloud.Client("gpt-neox-20b", secrets.NLPCLOUD, True)
+    # client = nlpcloud.Client("gpt-neox-20b", secrets.NLPCLOUD, True)
+    client = nlpcloud.Client("fast-gpt-j", secrets.NLPCLOUD, True)
+    story = []
     for i, part in enumerate(parts):
         if part.text == "<generate>":
-            prompt = create_prompt(parts[:i + 1])
+            prompt = create_prompt(story + [part])
             print(f"PROMPT: {prompt}", end="\n" + "-" * 80 + "\n")
-            part.text = client.generation(prompt,
+            text = client.generation(prompt,
                 min_length=10,
                 max_length=50,
                 length_no_input=True,
@@ -54,7 +76,10 @@ def fill_template_gpt(parts: List[Part]) -> List[Part]:
                 bad_words=None,
                 remove_end_sequence=False
             )['generated_text'].strip()
-    return parts
+            story.append(Part(part.raw, part.actor, part.media, text, True, part.voice))
+        else:
+            story.append(part)
+    return story
 
 
 def create_prompt(parts: List[Part]) -> str:
@@ -72,9 +97,8 @@ def create_prompt(parts: List[Part]) -> str:
     prompt += f"{parts[-1].actor}:"
     return prompt
 
-
-def generate_chapter(story_file, output):
-    """Open the story file and generate all media channels.
+def parse_template(story_file: str) -> List[Part]:
+    """Open the story file and return a list of parts.
 
     Format of the story_file:
     `
@@ -85,26 +109,32 @@ def generate_chapter(story_file, output):
         Room (video): bla
         Room (audio): bla
     `
-
-    Generates the following files:
-    `{output}/{actor}/{1,2,3}.{mp3,mp4}`
-
-    The processing pipeline is:
-    - replace <generate> with GPT generated text
-    - translate the text into German using deepl
-    - split the text into cues for each speaker
-    - generate audio or video for each speaker
-
-    Arguments:
-        story_file {str} -- path to the story file
-        output {str} -- path to the output dir
     """
     with open(story_file, "r") as f:
-        parts = [Part(i) for i in f.readlines()]
-    parts = fill_template_gpt(parts)
+        parts = [Part.parse(i) for i in f.readlines()]
+    return parts
+
+
+def write_parts(parts: List[Part], output_dir: str, filename: str) -> None:
+    os.makedirs(output_dir, exist_ok=True)
+    output = os.path.join(output_dir, filename)
     chapter = "\n".join(str(i) for i in parts)
-    chapter_de = translate(chapter)
-    print(chapter_de)
+    with open(output, "w") as f:
+        f.write(chapter)
+    return parts
+
+
+def translate_parts(parts: List[Part]) -> List[Part]:
+    """Translate all parts of the script
+    
+    Arguments:
+        parts {List[Part]} -- list of parts of the script
+    
+    Returns:
+        translated_parts {List[Part]} -- list of translated parts
+    """
+    return [i.translate() for i in parts]
+    
 
 
 def translate(text: str, lang: str = "DE") -> str:
@@ -121,6 +151,28 @@ def translate(text: str, lang: str = "DE") -> str:
     result = translator.translate_text(text, target_lang=lang) 
     translated_text = result.text
     return translated_text
+
+
+def text_to_media(parts, play, generate_all, target):
+    """Generate audio and video for each actor in the script
+    
+    Arguments:
+        story_out {str} -- path to the output dir
+        play {bool} -- whether to play the generated media
+        generate_all {bool} -- whether to generate all media
+    """
+    for i, part in enumerate(parts):
+        if not (part.generated or generate_all):
+            continue
+        if part.media == "audio":
+            if part.generated or generate_all:
+                if not play:
+                    actor_dir = f"{target}/{part.actor}"
+                    filename = os.path.join(actor_dir, f"/{i + 1}.mp3")
+                    os.system(f"say -v {part.voice} -o {filename} {part.text}")
+                else:
+                    os.system(f"say -v {part.voice} {part.text}")
+    
 
 @click.command()
 @click.argument("story_template", type=click.Path(exists=True))
@@ -139,8 +191,14 @@ def main(story_template: str, story_out: str, play: bool = False, generate_all: 
             (<generate> replacements)
     """
     os.makedirs(story_out, exist_ok=True)
-    for chapter in glob(f"{story_template}/*.txt"):
-        generate_chapter(chapter, chapter.replace(story_template, story_out))
+    for chapter in glob(f"{story_template}*.txt"):
+        target = chapter.replace(story_template, story_out).split(".txt")[0]
+        template = parse_template(chapter)
+        story = fill_template_gpt(template)
+        write_parts(story, target, "story_en.txt")
+        story_de = translate_parts(story)
+        write_parts(story_de, target, "story_de.txt")
+        text_to_media(story_de, play, generate_all, target)
 
 
 if __name__ == "__main__":
